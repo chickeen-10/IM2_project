08f7881074a6d526887b943fb043abdc84dead42
chickeen-10 <SABIDKENETH@GMAIL.COM>
2025-10-01 18:58:33 +0800

    HW3_sabid

---
+++ HW3_sabid/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_sabid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+
+            string[] words = input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                Dictionary<char, int> freq = GetFrequency(word);
+
+                List<string> output = new List<string>();
+                foreach (var item in freq)
+                {
+                    output.Add($"{item.Key}={item.Value}");
+                }
+                Console.WriteLine(string.Join(", ", output));
+            }
+
+            Console.ReadKey();
+        }
+
+        static Dictionary<char, int> GetFrequency(string str)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (dict.ContainsKey(c))
+                    dict[c]++;
+                else
+                    dict[c] = 1;
+            }
+            return dict;
+        }
+    }
+}
---
+++ SW1_sabid/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter row: ");
+        int row = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter col: ");
+        int col = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int [row, col];
+
+        Console.Write("Enter elements: ");
+
+        for (int i = 0; i < row; i++){
+            for(int j = row; j <col; j++)
+            {
+                Console.Write($"elements [{i + 1}, {j + 1}]");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.ReadKey();
+
+    }
+}
